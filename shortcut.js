5a71e09ed328dcf8a7ebd6a729dbfebf667152b1
CoderMahabub <codermahabub@gmail.com>
2021-09-12 18:15:49 +0600

    (Advanced) Truthy, False Ternary Operator, Shortcut And Or

---
+++ shortcut.js
@@ -0,0 +1,57 @@
+/* // TODO: Truthy Values Are: 'Almas', 5, true, {}, []
+// TODO: Falsy Values Are: '',0,false,null, undefined
+
+//Check Truthy
+let myVar = 5;
+// Check any Truthy
+if (myVar) {
+  myVar = myVar * 100;
+} else {
+  myVar = 0;
+}
+
+// Check Falsy anything or negative
+let myMoney = 50;
+if (!myMoney) {
+}
+
+const money = 800;
+let food;
+if (money > 100) {
+  food = "biriyani";
+} else {
+  food = "Cha Biscuit";
+}
+
+// TODO:02. ternary অপারেটর কি। এইটা দিয়ে শর্টকার্টে কিভাবে if-else লিখে
+let food1 = money > 100 ? "Biriyani" : "Biscuit";
+// console.log(food1);
+
+let drink = money > 100 && myVar > 100 ? "Coke" : "Filter Water";
+// console.log(drink);
+
+// Number to String Conversion
+const num1 = 52;
+// console.log(num1);
+const numStr = num1 + "";
+// console.log(numStr);
+
+// String to Number Conversion
+const input = "80";
+const inputNumber = +input;
+// console.log(inputNumber); */
+
+TODO:
+let isActive =true;
+const showUser = () =>console.log('Display User');
+const hideUser = () =>console.log('Hide User');
+// isActive ? showUser() : hideUser();
+
+// use && if the left side is true then right side will be executed
+isActive && showUser();
+
+// use || if the left side is false then right side will be executed
+isActive|| hideUser();
+
+//
+isActive=!isActive;
