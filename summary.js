7ea0a2f388bf2f7141512b14a564ba67cd6314a5
CoderMahabub <codermahabub@gmail.com>
2021-09-14 21:46:18 +0600

    Summary

---
+++ summary.js
@@ -0,0 +1,54 @@
+/* 
+1.1 varialbe (let, const) three types of variable
+1.2 condition (<,>, ===, !==, <=, >=, &&, ||, if-else)
+1.3 array: declare, length, index, push, pop, indexOf, includes, 
+1.4 for loop, while loop
+1.5 function return, parameter
+1.6 object: property, including array, object
+=============================================
+ES6
+1. template string ${}
+2. spread(...) 
+2.1 copy an array and then add a new element to an array
+2.2 use filter to remove an element from an array
+3. Arrow FUnction
+3.1 no parameter()
+3.2 singlie parameter
+3.3 multiple parameter()
+3.4 multi line 
+4. destructuring: object destructuring array destructuring
+5. object shorthand
+6. function parameter default value
+7. optional chaining ?.
+-----------------------------------------------
+Browser API
+1. Localstorate, sessionstorate
+2. location API
+3. History API
+4. Fetch
+================================================
+Others
+1. Array: map, forEach, filter, find
+2. Ternary operation
+3. Logican and, logical or
+4. JSON(stringify, parse)
+
+*/
+
+
+const x = 50;
+const y = 100;
+const obj = {
+    x:x, 
+    y:y
+};
+
+const obj1 ={x,y};
+
+const name = 'Solimullah';
+const area = 'Dhaka';
+const nobab ={
+    name: name,
+    area:area
+}
+const nobab2 ={name, area};
